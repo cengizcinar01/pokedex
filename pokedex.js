212c0be5ff11b346aebeafcd227d23fa832bf2e4
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-26 23:51:40 +0100

    Update pokedex.js with additional filters

--- pokedex.js
+++ pokedex.js
@@ -1,2 +1,6 @@
-const MAX_POKEDEX= 151;
+const MAX_POKEDEX = 151;
 const listWrapper = document.querySelector('.list-wrapper');
+const searchInput = document.querySelector('#search-input');
+const numberFilter = document.querySelector('#number');
+const nameFilter = document.querySelector('#name');
+const notFoundMessage = document.querySelector('#not-found-message');
