1ba2b8e8a4614c3e6d5e2738aa20cc4e5abd1f85
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-28 02:46:09 +0100

    Add input change handler to search.js
    This function toggles the visibility of the search close icon based on the value of the input element.

--- search.js
+++ search.js
@@ -5,3 +5,10 @@ const sortWrapper = document.querySelector('.sort-wrapper');
 inputElement.addEventListener('input', () => handleInputChange(inputElement));
 searchIcon.addEventListener('click', handleSearchCloseOnClick);
 sortWrapper.addEventListener('click', handleSortIconOnClick);
+
+function handleInputChange(inputElement) {
+    const inputValue = inputElement.value;
+    const searchCloseIcon = document.querySelector('#search-close-icon');
+
+    searchCloseIcon.classList.toggle('search-close-icon-visible', inputValue !== '');
+}
